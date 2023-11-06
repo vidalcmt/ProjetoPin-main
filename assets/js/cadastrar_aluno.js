3c6122873887bb2af80779d6e54bc26ecf0fb473
vidalcmt <144180174+vidalcmt@users.noreply.github.com>
2023-11-06 20:36:39 -0300

    ProjetoPin V4

---
+++ assets/js/cadastrar_aluno.js
@@ -0,0 +1,14 @@
+function prosseguirParaParte2() {
+    document.getElementById('parte1').style.display = 'none';
+    document.getElementById('parte2').style.display = 'block';
+}
+
+function prosseguirParaParte3() {
+    document.getElementById('parte2').style.display = 'none';
+    document.getElementById('parte3').style.display = 'block';
+}
+
+function prosseguirParaParte4() {
+    document.getElementById('parte3').style.display = 'none';
+    document.getElementById('parte4').style.display = 'block';
+}
