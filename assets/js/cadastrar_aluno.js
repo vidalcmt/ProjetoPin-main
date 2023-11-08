1fb9e939cfba3b11f2886df1ca8bbe79f5c5da2e
ProjetoMensal <fvcominetti@gmail.com>
2023-11-08 20:46:38 -0300

    Versão mais que atualiazada

--- assets/js/cadastrar_aluno.js
+++ assets/js/cadastrar_aluno.js
@@ -12,3 +12,18 @@ function prosseguirParaParte4() {
     document.getElementById('parte3').style.display = 'none';
     document.getElementById('parte4').style.display = 'block';
 }
+
+function mascara(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ 
+ }
