b64790b2091dfff8eb7859be077f780875c1082a
ProjetoMensal <fvcominetti@gmail.com>
2023-11-10 21:41:19 -0300

    Atualização Nova

--- assets/js/cadastrar_aluno.js
+++ assets/js/cadastrar_aluno.js
@@ -13,6 +13,20 @@ function mascara(i){
  
  }
 
+ function mascaraCep(i){
+   
+   var v = i.value;
+   
+   if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+      i.value = v.substring(0, v.length-1);
+      return;
+   }
+   
+   i.setAttribute("maxlength", "9");
+   if (v.length == 5) i.value += "-";
+
+}
+
  function mascaraRG(i){
    
     var v = i.value;
